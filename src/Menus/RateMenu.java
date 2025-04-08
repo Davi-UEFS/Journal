bf8981b102daf3ef5daed37b36e8657405ec43d0
Davi-UEFS <uefsdavi@gmail.com>
2025-04-07 23:37:37 -0300

    booleano success nao e mais necessario, pois o retorno é em String. Variaveis agora sao declaradas fora do escopo dos cases. Scanner e JournalController são final.

--- src/Menus/RateMenu.java
+++ src/Menus/RateMenu.java
@@ -1,12 +1,11 @@
 package Menus;
 
-import Prompts.*;
 import Journal.JournalController;
 import java.util.Scanner;
 
 public class RateMenu {
-    private Scanner scanner;
-    private JournalController journalController;
+    private final Scanner scanner;
+    private final JournalController journalController;
 
     public RateMenu(JournalController journalController, Scanner scanner){
         this.journalController = journalController;
@@ -16,6 +15,9 @@ public class RateMenu {
     public void rateMiniMenu() {
 
         int option;
+        String title, review;
+        int seasonNumber;
+        double rating;
 
         do {
             System.out.println(Prompts.Colors.blue + "--== MENU DE AVALIAÇÃO ==--" + Prompts.Colors.rst);
@@ -29,45 +31,34 @@ public class RateMenu {
 
             switch (option) {
                 case 1:
-                    String title = Prompts.AskInput.askForTitle(scanner);
-                    double rating = Prompts.AskInput.askForRate(scanner);
+                    title = Prompts.AskInput.askForTitle(scanner);
+                    rating = Prompts.AskInput.askForRate(scanner);
 
-                    boolean success = journalController.rateMedia(title, rating);
+                    System.out.println(journalController.rateMedia(title, rating));
 
-                    System.out.println((success) ? (Prompts.Colors.green + "Avaliação salva com sucesso!"+ Prompts.Colors.rst) :
-                            (Prompts.Colors.red + "Não foi possível salvar a avaliação!" + Prompts.Colors.rst));
                     break;
 
                 case 2:
-                    String title2 = Prompts.AskInput.askForTitle(scanner);
-                    String review = Prompts.AskInput.askForReview(scanner);
-
-                    boolean success2 = journalController.writeReview(title2, review);
-
-                    System.out.println((success2)? (Prompts.Colors.green + "Review salva com sucesso!"+ Prompts.Colors.rst) :
-                            (Prompts.Colors.red + "Não foi possível salvar a review!" + Prompts.Colors.rst));
+                    title = Prompts.AskInput.askForTitle(scanner);
+                    review = Prompts.AskInput.askForReview(scanner);
+                    System.out.println(journalController.writeMediaReview(title, review));
                     break;
 
                 case 3:
-                    String title3 = Prompts.AskInput.askForTitle(scanner);
-                    int season_n = Prompts.AskInput.askForSeasonNumber(scanner);
-                    double rating2 = Prompts.AskInput.askForRate(scanner);
+                    title = Prompts.AskInput.askForTitle(scanner);
+                    seasonNumber = Prompts.AskInput.askForSeasonNumber(scanner);
+                    rating = Prompts.AskInput.askForRate(scanner);
 
-                    boolean success3 = journalController.rateSeason(title3, season_n, rating2);
+                    System.out.println(journalController.rateSeason(title, seasonNumber, rating));
 
-                    System.out.println((success3)? (Prompts.Colors.green + "Avaliação salva com sucesso!"+ Prompts.Colors.rst) :
-                            (Prompts.Colors.red + "Não foi possível salvar a avaliação!" + Prompts.Colors.rst));
                     break;
 
                 case 4:
-                    String title4 = Prompts.AskInput.askForTitle(scanner);
-                    int season_n2 = Prompts.AskInput.askForSeasonNumber(scanner);
-                    String review2 = Prompts.AskInput.askForReview(scanner);
+                    title = Prompts.AskInput.askForTitle(scanner);
+                    seasonNumber = Prompts.AskInput.askForSeasonNumber(scanner);
+                    review = Prompts.AskInput.askForReview(scanner);
 
-                    boolean success4 = journalController.reviewSeason(title4, season_n2, review2);
-
-                    System.out.println((success4)? (Prompts.Colors.green + "Review salva com sucesso!"+ Prompts.Colors.rst) :
-                            (Prompts.Colors.red + "Não foi possível salvar a review!" + Prompts.Colors.rst));
+                    System.out.println(journalController.writeSeasonReview(title, seasonNumber, review));
                     break;
 
                 default:
