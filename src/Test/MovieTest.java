3e0d7e2badaaf488e57e9a98e95a1fc829e63eb4
Davi-UEFS <uefsdavi@gmail.com>
2025-04-09 14:55:44 -0300

    Adicionado teste para filmes

---
+++ src/Test/MovieTest.java
@@ -0,0 +1,40 @@
+package Test;
+
+import Controller.JournalController;
+import Model.Library;
+
+import Model.Media.Movie;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
+import java.util.ArrayList;
+
+public class MovieTest {
+
+    @Test
+    public void addMovieTest(){
+        Library journal = new Library();
+        JournalController journalController = new JournalController(journal);
+
+        journalController.register(
+                "Aventuras no Espaço", 2021, 2, new String[]{"Carlos", "Mariana", "João"},
+                Duration.ofMinutes(125), "Maria Silva", "Roteiro etc etc",
+                "Space Adventures", new String[]{"Netflix", "HBO Max"}
+        );
+
+        journalController.register(
+                "Mistério na Floresta", 2022, 4, new String[]{"Lucas", "Clara", "Paulo"},
+                Duration.ofMinutes(98), "Fernanda Costa", "Roteiro etc etc",
+                "Forest Mystery", new String[]{"Amazon Prime", "GloboPlay"}
+        );
+
+        assertEquals(2, journalController.allMovies().size());
+
+        printMovieList(journalController.allMovies());
+    }
+
+    private void printMovieList(ArrayList<Movie> movieList){
+        movieList.forEach(movie -> System.out.println(movie.toString()));
+    }
+}
