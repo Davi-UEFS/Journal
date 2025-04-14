f31c58f9e2829e846e1f5f071125a1fc1907923f
Davi-UEFS <uefsdavi@gmail.com>
2025-04-14 16:46:02 -0300

    Implementa IResult. Metodo markAsSeen (temporada) adicionado. Metodos rateSeason e writeReviewSeason checam se a temporada ja foi vista e retornam em IResult. Removida busca interna, ja recebe a serie por parametro. Metodos readReviewSeason, ShowRatingSeason e ShowRating recebem a serie diretamente.

--- src/Controller/SeriesService.java
+++ src/Controller/SeriesService.java
@@ -1,13 +1,13 @@
 package Controller;
 
 import Model.Exceptions.MediaAlreadyExistsException;
-import Model.Exceptions.MediaNotFoundException;
 import Model.Exceptions.SeasonNotFoundException;
 import Model.Genres;
 import Model.Library;
-import Model.Medias.Media;
+
 import Model.Medias.Season;
 import Model.Medias.Series;
+import Model.Result.*;
 
 import java.util.*;
 
@@ -17,7 +17,7 @@ public class SeriesService extends CommonService<Series> {
         super(journal);
     }
 
-    public String register(String title, int year, Genres genre, int yearOfEnding,
+    public IResult register(String title, int year, Genres genre, int yearOfEnding,
                            String[] castBuffer, String originalTitle, String[] whereToWatchBuffer,
                            int seasonNumber, int episodeCount){
 
@@ -34,88 +34,95 @@ public class SeriesService extends CommonService<Series> {
             series.addSeason(season);
             journal.addSeries(series);
             journal.addYear(year);
-            return "Série registrada com sucesso ✔";
+            return new Success("Série","Registrada com sucesso.");
         }catch (MediaAlreadyExistsException e){
-            return e.getMessage();
+            return new Failure("Série",e.getMessage());
         }
     }
 
-    /**
-     * Metodo rate não deve ser usado. A atualização já é feita no rateSeason
-     * */
-    @Override
-    public String rate(String title, double rating){
-        try {
-            Series series = journal.findSeries(title);
-            series.updateRate();
-
-            return "Avaliação atualizada com sucesso";
-
-        } catch (MediaNotFoundException e) {
-            return e.getMessage();
-        }
+    public IResult registerSeason(Series series, int seasonNumber, int episodeCount){
+        Season season = new Season(seasonNumber, episodeCount);
+        try{
+            series.findSeason(seasonNumber);
+            return new Failure("Temporada", "Já existe");
+        } catch (SeasonNotFoundException e){
+            series.addSeason(season);
+            return new Success("Temporada", "Registrada com sucesso.");
+        } //TODO: MUDAR ANO DA SERIE?
     }
 
-    public String rateSeason(String title, int seasonNumber, double rating) {
+    public IResult markAsSeen(Series series, int seasonNumber) {
+        try{
+            Season season = series.findSeason(seasonNumber);
+            if(season.isSeen()) {
+                return new Failure("Temporada", "Já marcado como visto");
+            }
+            season.setSeen(true);
+            return new Success("Temporada", "Marcado como visto");
+
+    }catch (SeasonNotFoundException e){
+        return new Failure("Temporada",e.getMessage());
+    }
+    }
+
+    //TODO: OVERRIDE EM RATE E REVIEW DO PAI. EXCECAO
+           /////////////////////////////////////////////
+    public IResult rateSeason(Series series, int seasonNumber, double rating) {
         try {
-            Series series = journal.findSeries(title);
 
             Season season = series.findSeason(seasonNumber);
-            season.setRating(rating);
-            series.updateRate();
-            return "Avaliação salva com sucesso";
+            if(season.isSeen()) {
+                season.setRating(rating);
+                series.updateRate();
+                return new Success("Temporada", "Avaliação salva com sucesso");
+            }
+            return new Failure("Temporada", "Marque como visto antes de avaliar");
 
-        }catch (SeasonNotFoundException | MediaNotFoundException e){
-            return e.getMessage();
+        }catch (SeasonNotFoundException e){
+            return new Failure("Temporada",e.getMessage());
         }
     }
 
-    public String writeReviewSeason(String title, int seasonNumber, String review) {
+    public IResult writeReviewSeason(Series series, int seasonNumber, String review) {
         try {
-            Series serie = journal.findSeries(title);
-            Season season = serie.findSeason(seasonNumber);
-            season.setReview(review);
-            return "Review salva com sucesso";
-
-        } catch (SeasonNotFoundException | MediaNotFoundException e) {
-            return e.getMessage();
+            Season season = series.findSeason(seasonNumber);
+            if(season.isSeen()) {
+                season.setReview(review);
+                return new Success("Temporada", "Review salva com sucesso");
+            }
+            return new Failure("Temporada", "Marque como visto antes de escrever uma review");
+        } catch (SeasonNotFoundException e) {
+            return new Failure("Temporada",e.getMessage());
         }
-
     }
 
-    public String readReviewSeason(String title, int seasonNumber) {
+    public String readReviewSeason(Series series, int seasonNumber) {
 
         try {
-            Series series = journal.findSeries(title);
+
             Season season = series.findSeason(seasonNumber);
             return "Review: " + ((season.getReview() == null) ?
                     "Você ainda não escreveu uma review desta temporada" : season.getReview());
-        } catch (MediaNotFoundException e) {
+        } catch (SeasonNotFoundException e) {
             return e.getMessage();
 
         }
     }
     @Override
-    public String showRating(String title) {
+    public String showRating(Series series) {
 
-        try {
-            Media media = journal.findMedia(title);
-            return "Nota geral: " + ((media.getRating() == 0.0) ?
-                    "Sem temporadas avaliadas" : media.getRating());
-        } catch (MediaNotFoundException e) {
-            return e.getMessage();
+            return "Nota geral: " + ((series.getRating() == 0.0) ?
+                    "Sem temporadas avaliadas" : series.getRating());
 
-        }
     }
 
-    public String showRatingSeason(String title, int seasonNumber) {
+    public String showRatingSeason(Series series, int seasonNumber) {
 
         try {
-            Series series = journal.findSeries(title);
             Season season = series.findSeason(seasonNumber);
             return "Nota: " + ((season.getRating() == 0.0) ?
                     "Você ainda não avaliou esta temporada" : season.getRating());
-        } catch (MediaNotFoundException e) {
+        } catch (SeasonNotFoundException e) {
             return e.getMessage();
 
         }
