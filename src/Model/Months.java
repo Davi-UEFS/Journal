75a88e1d0eba736b52acfced0709757137651a32
GuiLima08 <guilima.oliver@gmail.com>
2025-04-17 23:16:21 -0300

    Criei Enum Months

---
+++ src/Model/Months.java
@@ -0,0 +1,16 @@
+package Model;
+
+public enum Months {
+    JANEIRO,
+    FEVEREIRO,
+    MARÇO,
+    APRIL,
+    MAIO,
+    JUNHO,
+    JULHO,
+    AGOSTO,
+    SETEMBRO,
+    OUTUBRO,
+    NOVEMBRO,
+    DEZEMBRO
+}
